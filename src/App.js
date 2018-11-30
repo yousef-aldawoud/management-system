90371f5a99681c267daf981eb735025a4fb35b31
Yousef <ice.ghost.ig@gmail.com>
2018-11-30 17:26:00 +1300

    Made a prohects table for small screens

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ class App extends Component {
  state = {
   projects:[
     {name:"Zero Xl kk",manager:"Jhon",due_date:"10/7/2018",status:"finished"},
-    {name:"Devolp a website",manager:"Macenize",due_date:"1/19/2018",status:"unfinished"},
+    {name:"Devolp a website",manager:"Macenize hohoho",due_date:"1/19/2018",status:"unfinished"},
     {name:"Handle requests",manager:"Jesus Christ",due_date:"12/12/2018",status:"unfinished"},
     {name:"Handle requests",manager:"Jesus Christ",due_date:"12/12/2018",status:"unfinished"},
 
--- src/Projects.js
+++ src/Projects.js
@@ -3,48 +3,79 @@ import NavBarStyle from   './NavBar.css';
 import   './projects.css';
 import {Button,NavItem,Dropdown,Navbar,Collection,CollectionItem} from 'react-materialize'
 import SideNavBar from './SideNavBar';
-class Projects extends Component {
-   state ={
+const Projects = ({projects}) =>{
    
-    }
-    getProjects(project,index){
-        var dueDate = new Date(project.due_date);
+    const getStatus = (due_date,project_status)=>{
+        var dueDate = new Date(due_date);
         var currentDate = new Date();
-        if(project.status=="finished"){
+        if(project_status=="finished"){
             var status =  (<span className="badge blue white-text status">finished</span>);
         }
-        else if(dueDate<currentDate && project.status=="unfinished"){
+        else if(dueDate<currentDate && project_status=="unfinished"){
             var status =  (<span className="badge red white-text status">overdue</span>);;
         }else{
             var status = (<span className="badge green white-text status"><div>ongoing</div></span>);
         }
-        
+        return status
+    }
+    const getProjectsOnSmallScreen = (project,index)=>{
+        var status = getStatus(project.due_date,project.status);
+        var dueDate = new Date(project.dueDate);
         return(
-          <CollectionItem className="row row_stick white">
-                <p className="col s2 center row_stick"><p className="grey-text row_stick">Project title</p>{project.name}</p>
-                <p className="col s2 center offset-s3 row_stick"><p className="grey-text row_stick">person</p>{project.manager}</p>
-                <p className="col s1 center offset-s1 row_stick"><p className="grey-text row_stick">due date</p>{dueDate.toLocaleDateString("en-NZ")}</p>
-                <p className="col s2 center offset-s1">{status}<a href="#" className="link small">details</a></p>
+            <tr>
+            <td>{project.name}</td>
+            <td>{project.manager}</td>
+            <td>{status}</td>
+            <td><a className="link" href="#">details</a></td>
+          </tr>
+        )
+       
+    }
+    const getProjects= (project,index)=>{
+        var status = getStatus(project.due_date,project.status);
+        var dueDate = new Date(project.dueDate);
+        return(
+          <CollectionItem className="row row_stick white" key={index}>
+                <p className="col l2 m2 center row_stick"><p className="grey-text row_stick">Project title</p>{project.name}</p>
+                <p className="col l2 m2 center offset-l2 offset-m1 row_stick"><p className="grey-text row_stick">person</p>{project.manager}</p>
+                <p className="col l2 m2 center offset-l1 offset-m1 row_stick"><p className="grey-text row_stick">due date</p>{dueDate.toLocaleDateString("en-NZ")}</p>
+                <p className="col l1 m1 center offset-l1 offset-m1"><a href="#" className="link small">details</a></p>
+                <p className="col l1 m1 center offset-m1">{status}</p>
             </CollectionItem>
             );
     }
-  render() {
-      this.output = this.props.projects.length ? (this.props.projects.map(this.getProjects)): (<p className="center">"Nothing to do"</p>);
+  
+    const output = projects.length ? (projects.map(getProjects)): (<p className="center">"Nothing to do"</p>);
+    const outputForSmall = projects.length ? (projects.map(getProjectsOnSmallScreen)): (<p className="center">"Nothing to do"</p>);
     return (
       <div className="container">
           <div className="row">
           <h4 className="grey-text text-darken-2 offset-l1" >Dashboard</h4>
           </div>
-          <Collection header='Projects'>
-          
-          {this.output}
+          <Collection header="Projects" className ="hide-on-small-only">
+
+          {output}
           
         
             </Collection>
             
+            <table className="highlight responsive hide-on-med-and-up white">
+                <thead>
+                <tr>
+                    <th>Project title</th>
+                    <th>Person</th>
+                    <th>Status</th>
+                    <th>#</th>
+                </tr>
+                </thead>
+
+                <tbody>
+                    {outputForSmall}
+                </tbody>
+      </table>
       </div>
     );
-  }
+  
 }
 
 export default Projects;
